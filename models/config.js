a479c8c2d9d698a5962b33a9389f49efd20149d7
Sumon <sumonkhan1081@gmail.com>
2023-11-16 15:12:13 +0530

    Cloud database save

--- models/config.js
+++ models/config.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const url = process.env.URL;
 
 mongoose
-    .connect(url)
+    .connect(url,{
+        useUnifiedTopology: true,
+    })
     .then(()=>{ console.log('DB Connected'); })
     .catch((err)=>{ console.log(err); })
