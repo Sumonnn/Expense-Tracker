6b71e8dcb035b24de494d203d923c535f7bf81bb
Sumon <sumonkhan1081@gmail.com>
2023-11-16 17:30:36 +0530

    Host Testing

--- models/config.js
+++ models/config.js
@@ -3,10 +3,7 @@ const url = process.env.URL;
 
 mongoose
     .connect(url,{
-        useNewUrlParser:true,
-        useCreateIndex:true,
         useUnifiedTopology: true,
-        useFindAndModify:false,
     })
     .then(()=>{ console.log('DB Connected'); })
     .catch((err)=>{ console.log(err); })
